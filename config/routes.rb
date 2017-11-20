225fdec0314268f2ad0dad7502391a460092e23a
Scott Munro <simunro@hotmail.co.uk>
2017-11-20 21:57:12 +0100

    deleted static_pages and modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   get 'simple_pages/contact'
 
-  get 'static_pages/index'
-  root 'static_pages#index'
+  get 'simple_pages/index'
+	root 'simple_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
