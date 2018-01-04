274d807703cfbb54d64438b96103cf077d44365e
Scott Munro <simunro@hotmail.co.uk>
2018-01-04 19:48:18 +0100

    5.2 added search form to navbar. Styled products forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ApplicationRecord
 	has_many :orders
 
-	def self.search(search_term)
-  	Product.where("name LIKE ?", "%#{search_term}%")
-	end
+	 def self.search(search_term)
+    like_operator = Rails.env.production? ? 'ilike' : 'like'
+    Product.where("name #{like_operator} ?", "%#{search_term}%")
+  end
 end
 
 
