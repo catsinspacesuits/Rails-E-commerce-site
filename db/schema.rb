32989558e637ccca7f71dc8f6fbe5669f409a908
Scott Munro <simunro@hotmail.co.uk>
2018-01-10 16:19:44 +0100

    redid migrations after recreating scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108192125) do
+ActiveRecord::Schema.define(version: 20180110135423) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20180108192125) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
+    t.decimal "price"
   end
 
 end
