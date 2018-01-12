856058a73ebd151a48d06b54d9d00b7a229079a4
Scott Munro <simunro@hotmail.co.uk>
2018-01-12 03:16:08 +0100

    updated thank_you method to simple pages controller

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -15,7 +15,10 @@ class SimplePagesController < ApplicationController
 	  @name = params[:name]
 	  @email = params[:email]
 	  @message = params[:message]
-	  UserMailer.contact_form(@email, @name, @message).deliver_now
+	  ActionMailer::Base.mail(from: @email,
+	      to: 'your-email@example.com',
+	      subject: "A new contact form message from #{@name}",
+	      body: @message).deliver_now
 	end
 
  
