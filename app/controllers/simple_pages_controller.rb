b4b4281c92bd57b3f3561117cf211997f0c58b19
Scott Munro <simunro@hotmail.co.uk>
2018-01-12 03:22:18 +0100

    added default email for contact form

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -15,10 +15,7 @@ class SimplePagesController < ApplicationController
 	  @name = params[:name]
 	  @email = params[:email]
 	  @message = params[:message]
-	  ActionMailer::Base.mail(from: @email,
-	      to: 'your-email@example.com',
-	      subject: "A new contact form message from #{@name}",
-	      body: @message).deliver_now
+	  UserMailer.contact_form(@email, @name, @message).deliver_now
 	end
 
  
