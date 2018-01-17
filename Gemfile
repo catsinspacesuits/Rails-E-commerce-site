a1375be5862c7ed154aafc97f8f1006bd8552a39
Scott Munro <simunro@hotmail.co.uk>
2018-01-17 02:21:40 +0100

    5.9 added validation to products/new form. added pagination to product comments

--- Gemfile
+++ Gemfile
@@ -42,3 +42,4 @@ end
 
 gem 'devise'
 gem 'cancancan', '~> 2.0'
+gem 'will_paginate', '~> 3.1.0'
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,12 @@
 class Comment < ApplicationRecord
+	validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }
+
   belongs_to :user
   belongs_to :product
+  
   scope :rating_desc, -> { order(rating: :desc) }
   scope :rating_asc, -> { order(rating: :asc) }
 end
