68a8297993f876448aa2cc4992ff568f6384047e
Scott Munro <simunro@hotmail.co.uk>
2018-01-25 15:07:59 +0100

    amended payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,19 +1,19 @@
 class PaymentsController < ApplicationController
 	def create
-		@product = params[:product_id]
+		@product = param.find[:product_id]
 		@user = current_user
 		token = params[:stripeToken]
 	  # Create the charge on Stripe's servers - this will charge the user's card
 	  begin
 	    charge = Stripe::Charge.create(
-	      amount: @product.price 
+	      amount: (@product.price*100) 
 	      currency: "usd",
 	      source: token,
 	      description: params[:stripeEmail]
 	    )
 	  if charge.paid
 	    Order.create(product_id: @product.id, user_id: @user.id,
-      total: @product.price.to_i)
+      total: @product.price)
 	  end
 
 	  rescue Stripe::CardError => e
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get 'simple_pages/thank_you'
   get 'simple_pages/index'
 	root "simple_pages#landing_page"
-  post "payments/create"
+  post 'payments/create'
 
 	resources :orders, only: [:index, :show, :create, :destroy]
 
