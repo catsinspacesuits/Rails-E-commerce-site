dcdf894b57f453a41fe61410ca0d742418dbc883
Scott Munro <simunro@hotmail.co.uk>
2018-01-25 11:56:16 +0100

    6.6 added payments controller and relevant routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,12 @@ Rails.application.routes.draw do
   resources :users
   
   post 'simple_pages/thank_you'
-  
   get 'simple_pages/about'
-
   get 'simple_pages/contact'
-
   get 'simple_pages/thank_you'
-
   get 'simple_pages/index'
 	root "simple_pages#landing_page"
+  post "payments/create"
 
 	resources :orders, only: [:index, :show, :create, :destroy]
 
