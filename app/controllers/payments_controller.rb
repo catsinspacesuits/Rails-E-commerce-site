4d31663c701a9162a93553d088b9593b80012ae5
Scott Munro <simunro@hotmail.co.uk>
2018-01-26 19:47:13 +0100

    fixed payments controller bug

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,6 @@
 class PaymentsController < ApplicationController
 	def create
-		@product = param.find[:product_id]
+		@product = Product.find(params[:product_id])
 		@user = current_user
 		token = params[:stripeToken]
 	  # Create the charge on Stripe's servers - this will charge the user's card
