2ac20c2599f4f08c9574cf732a4112954a2f1ea1
Scott Munro <simunro@hotmail.co.uk>
2018-01-29 11:15:31 +0100

    changed payments controller to try and fix bug

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,29 +1,26 @@
 class PaymentsController < ApplicationController
-	def create
-		token = params[:stripeToken]
-		@product = Product.find(params[:product_id])
-		@user = current_user
-		
-	  # Create the charge on Stripe's servers - this will charge the user's card
-	  begin
-	    charge = Stripe::Charge.create(
-	      amount: (@product.price*100), 
-	      currency: "usd",
-	      source: token,
-	      description: params[:stripeEmail]
-	    )
-	  if charge.paid
-	    Order.create(product_id: @product.id, user_id: @user.id,
-      total: @product.price)
-	  end
+  def create
+    token = params[:stripeToken]
+    @product = Product.find(params[:product_id])
+    @user = current_user
 
-	  rescue Stripe::CardError => e
-	    # The card has been declined
+    # Create the charge on Stripe's servers to charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        amount: (@product.price*100).to_i, currency: "usd", source: token, description: params[:stripeEmail]
+      )
+
+      if charge.paid
+        Order.create!(product_id: @product.id, user_id: @user.id, total: @product.price.to_i)
+        UserMailer.order_placed(@user,@product).deliver_now
+      end
+
+    rescue Stripe::CardError => e
       body = e.json_body
       err = body[:error]
-      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-	  end
-		redirect_to product_path(@product), notice: 'Thank you for your order!'
-	end
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}" 
+    end
 
+    redirect_to product_path(@product), notice: 'Thank you for your order!'
+  end
 end
