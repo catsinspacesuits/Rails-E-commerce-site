77c69db1004cb07b683029a2f07086e5923d9221
Scott Munro <simunro@hotmail.co.uk>
2018-01-29 18:36:50 +0100

    changed payment controller to fix bug

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,5 @@
 class PaymentsController < ApplicationController
-	protect_from_forgery with: :null_session
+	skip_before_action :verify_authenticity_token
 	def create
 		token = params[:stripeToken]
 		@product = Product.find(params[:product_id])
