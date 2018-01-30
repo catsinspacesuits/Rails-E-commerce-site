adfde772c38cee0965b8b41804c1c82ea1bad619
Scott Munro <simunro@hotmail.co.uk>
2018-01-30 12:04:15 +0100

    6.13 set up ActionCable and products cable. Set up streaming and broadcasting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://redandblackbooks.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://redandblackbooks.herokuapp.com', 'http://redandblackbooks.herokuapp.com']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :users
+  resources :orders, only: [:index, :show, :create, :destroy]
   
   post 'simple_pages/thank_you'
   get 'simple_pages/about'
@@ -14,7 +15,8 @@ Rails.application.routes.draw do
 	root "simple_pages#landing_page"
   post 'payments/create'
 
-	resources :orders, only: [:index, :show, :create, :destroy]
+  mount ActionCable.server => '/cable'
 
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
