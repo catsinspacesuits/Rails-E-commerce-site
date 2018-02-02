fa029f8c232338df62becd2ba74acffc2b2c4c4d
Scott Munro <simunro@hotmail.co.uk>
2018-02-03 00:33:42 +0800

    6.14 authenticated ActionCable connections and added action job

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,28 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+    
+    end
+
+    def disconnect 
+    
+    end
   end
+
+  protected
+  
+    def find_verified_user
+      if verified_user = request.env['warden'].user
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+    
+    def connect
+      self.current_user = find_verified_user
+    end
+
 end
